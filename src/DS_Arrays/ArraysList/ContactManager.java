322d4801ed0f731f1a97538525d5e775d4bbcff7
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-02 19:29:56 -0500

    Build the skeleton for the ContactManager program

--- src/DS_Arrays/ArraysList/ContactManager.java
+++ src/DS_Arrays/ArraysList/ContactManager.java
@@ -7,9 +7,30 @@ Display all contacts in the array.
  */
 package DS_Arrays.ArraysList;
 
+import java.util.ArrayList;
+
 public class ContactManager {
 
+    public static String addContacts(){
+        return "";
+    }
+
+    public static String searchContact(){
+        return "";
+    }
+
+    public static void displayContacts(){
+        System.out.println("Contacts: ");
+    }
+
+    public static int menu(){
+        return 0;
+    }
+
     public static void main(String[] args) {
+
+        ArrayList<String> contactManager = new ArrayList<>();
+
 
     }
 }
