bd6f2b16fb697d4d3bd6db2cd38a8fad52a17550
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-10 13:41:10 -0500

    Refactor code

--- src/DS_Stacks/Implementation/stackImplement.java
+++ src/DS_Stacks/Implementation/stackImplement.java
@@ -1,89 +1,76 @@
 package DS_Stacks.Implementation;
+/* Java program to implement basic stack
+operations */
 
-class stackImplement {
+class StackImplement {
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-        // Store elements of stack
-        private int[] arr;
+    boolean isEmpty()
+    {
+        return (top < 0);
+    }
+    StackImplement()
+    {
+        top = -1;
+    }
 
-        // Represent top of stack
-        private int top;
-
-        // Total capacity of the stack
-        private int capacity;
-
-        // Creating a stack
-        stackImplement(int size) {
-            // Initialize the array
-            arr = new int[size];
-            capacity = size;
-            top = -1;
+    boolean push(int x)
+    {
+        if (top >= (MAX - 1)) {
+            System.out.println("Stack Overflow");
+            return false;
         }
-
-        // Push elements to the top of stack
-        public void push(int x) {
-            if (isFull()) {
-                System.out.println("Stack Overflow");
-
-                // Terminates the program
-                System.exit(1);
-            }
-
-            // Insert element on top of the stack
-            System.out.println("Inserting " + x);
-            arr[++top] = x;
-        }
-
-        public int pop() {
-            // If stack is empty
-            if (isEmpty()) {
-                System.out.println("Empty Stack");
-
-                // Terminate the program
-                System.exit(1);
-            }
-
-            // Pop the element from top of stack
-            return arr[top--];
-        }
-
-        // Return size of the stack
-        public int getSize() {
-            return top + 1;
-        }
-
-        // Check if the stack is empty
-        public Boolean isEmpty() {
-            return top == -1;
-        }
-
-        // Check if the stack is full
-        public boolean isFull() {
-            return top == capacity - 1;
-
-        }
-
-        // Display elements of stack
-        public void printStack() {
-            for (int i = 0; i <= top; i++) {
-                System.out.println(arr[i] + " ");
-            }
-        }
-
-        public static void main(String[] args) {
-            stackImplement stack = new stackImplement(10);
-
-            // Adding elements to the stack
-            stack.push(1);
-            stack.push(3);
-            stack.push(5);
-
-            System.out.println("Stack: ");
-            stack.printStack();
-
-            // Remove elements from stack
-            stack.pop();
-
-            System.out.println("After popping out");
-            stack.printStack();
+        else {
+            a[++top] = x;
+            System.out.println(x + " pushed into stack");
+            return true;
         }
     }
+
+    int pop()
+    {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int peek()
+    {
+        if (top < 0) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        else {
+            int x = a[top];
+            return x;
+        }
+    }
+
+    void print(){
+        for(int i = top;i>-1;i--){
+            System.out.print(" "+ a[i]);
+        }
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        StackImplement s = new StackImplement();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Top element is :" + s.peek());
+        System.out.print("Elements present in stack :");
+        s.print();
+    }
+}
