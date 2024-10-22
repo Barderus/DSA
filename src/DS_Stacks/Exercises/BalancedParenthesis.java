75600a9bbbb874dd241489f583e354be8fe6b6a5
Gabriel dos Reis <dosreisg@dupage.edu>
2024-10-22 15:55:24 -0500

    Practice with Stacks

---
+++ src/DS_Stacks/Exercises/BalancedParenthesis.java
@@ -0,0 +1,15 @@
+package DS_Stacks.Exercises;
+
+import java.util.Stack;
+
+/*
+    Write a function that checks if a string has balanced parentheses (e.g., (), [], {}).
+    Use a stack to track opening parentheses.
+ */
+public class BalancedParenthesis {
+
+    public static void main(String[] args) {
+
+        Stack<Character> stack = new Stack<>();
+    }
+}
---
+++ src/DS_Stacks/Exercises/SortStack.java
@@ -0,0 +1,7 @@
+package DS_Stacks.Exercises;
+
+/*
+    Write a function that sorts a stack using another stack. The elements should be sorted in ascending order.
+ */
+public class SortStack {
+}
